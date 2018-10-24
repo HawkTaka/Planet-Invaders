25861ed868aaee2f8bb18f3d966dab5ef5bde29c
Charles Hapgood Strickland <hawk.mirlen@gmail.com>
2018-10-24 22:11:03 +0200

    Path Finding + Load Grid Objects + Remove Duplicate Blocks

---
+++ Assets/PathFinder.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PathFinder : MonoBehaviour {
+
+
+    Dictionary<Vector2Int, Waypoint> grid = new Dictionary<Vector2Int, Waypoint>();
+
+
+	// Use this for initialization
+	void Start () {
+        LoadBlocks();
+	}
+
+    private void LoadBlocks()
+    {
+        var waypointList = FindObjectsOfType<Waypoint>();
+
+        foreach (Waypoint waypoint in waypointList)
+        {
+            bool isOverLapping = grid.ContainsKey(waypoint.GetGridPos());
+
+            if (isOverLapping)
+            {
+                Debug.LogWarning("SKipping overlapping block " + waypoint);
+            }
+            else
+            {
+                grid.Add(waypoint.GetGridPos(), waypoint);
+            }
+        }
+
+        print("Loaded Grid Total blocks :" + grid.Count);
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
--- Assets/Waypoint.cs
+++ Assets/Waypoint.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -6,12 +7,13 @@ public class Waypoint : MonoBehaviour {
     const int GridSize = 10;
     Vector2Int GridPos;
 
-    public Vector2  GetGridPos()
+    public Vector2Int  GetGridPos()
     {
-        return new Vector2(
+        return new Vector2Int(
          Mathf.RoundToInt(this.transform.position.x / GridSize) * GridSize,
          Mathf.RoundToInt(this.transform.position.z / GridSize) * GridSize);
     }
+
 
     public int GetGridSize()
     {
