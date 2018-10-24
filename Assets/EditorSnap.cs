90f5637d41e2811a087c9e87d9c1e0247238eaf3
Charles Hapgood Strickland <hawk.mirlen@gmail.com>
2018-10-24 19:41:34 +0200

    Create Project in unity, Create Cube Add Editor Snap Script

---
+++ Assets/EditorSnap.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[ExecuteInEditMode]
+public class EditorSnap : MonoBehaviour {
+
+    [SerializeField] [Range(1f,20f)] float GridSize = 10f;
+
+	// Update is called once per frame
+	void Update () {
+        Vector3 snap = new Vector3();
+        snap.x = Mathf.RoundToInt(this.transform.position.x / GridSize) * GridSize;
+        snap.z = Mathf.RoundToInt(this.transform.position.z / GridSize) * GridSize;
+        
+
+        transform.position = new Vector3(snap.x, 0, snap.z);
+	}
+}
