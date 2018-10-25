728d7eda4a0db121a3dd18a068333d95eed042ea
Charles Hapgood Strickland <hawk.mirlen@gmail.com>
2018-10-25 16:32:42 +0200

    Basic Tower Logic

---
+++ Assets/Tower.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tower : MonoBehaviour {
+
+    Enemy enemy;
+    [SerializeField] public GameObject top;
+
+
+    private void Start()
+    {
+        enemy = FindObjectOfType<Enemy>();
+    }
+
+
+    // Update is called once per frame
+    void Update () {
+        top.transform.LookAt(enemy.transform);
+
+
+	}
+}
