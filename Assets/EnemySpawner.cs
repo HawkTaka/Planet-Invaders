ddeeb15d6ae2f840b1f4d25f5e52cb8863642256
Charles Hapgood Strickland <hawk.mirlen@gmail.com>
2018-10-27 17:14:53 +0200

    Add Spawnner script to spawn new Enemies

---
+++ Assets/EnemySpawner.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour {
+
+    [SerializeField] [Range(0.1f, 120f)] float SecondsBetweenSpawns = 4f;
+    [SerializeField] public GameObject EnemyToSpawn;
+
+
+    private void Start()
+    {
+       if (EnemyToSpawn == null)
+        {
+            print("EnemySpawner: noting to spawn");
+            return;
+        }
+
+
+
+            StartCoroutine(Spawn());
+      
+
+    }
+
+
+
+    IEnumerator Spawn()
+    {
+        while (true)
+        {
+            Instantiate(EnemyToSpawn, transform.position, Quaternion.identity, gameObject.transform);
+            yield return new WaitForSeconds(SecondsBetweenSpawns); 
+        }
+    }
+
+
+}
