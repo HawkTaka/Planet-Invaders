51cc30defd56f8197ffe5617e409d8109179639e
Charles Hapgood Strickland <hawk.mirlen@gmail.com>
2018-10-27 16:24:51 +0200

    Tower Logic added (Max Shoot distance) Follow closest Enemy

---
+++ Assets/EnemyDamage.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyDamage : MonoBehaviour {
+
+    [SerializeField] public int HitPoints = 5;
+
+    private void OnParticleCollision(GameObject other)
+    {
+        ProcessHit();
+    }
+
+    private void ProcessHit()
+    {
+        HitPoints--;
+        if (HitPoints <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
--- Assets/Tower.cs
+++ Assets/Tower.cs
@@ -1,23 +1,57 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class Tower : MonoBehaviour {
-
-    Enemy enemy;
     [SerializeField] public GameObject top;
+    [SerializeField] public float Range = 10f;
+    [SerializeField] ParticleSystem Guns;
 
+
+    private Enemy target;
 
     private void Start()
     {
-        enemy = FindObjectOfType<Enemy>();
+        Guns = FindObjectOfType<ParticleSystem>();
     }
 
 
     // Update is called once per frame
     void Update () {
-        top.transform.LookAt(enemy.transform);
 
-
+        FindTraget();
+        FocusOnTarget();
 	}
+
+    private void FocusOnTarget()
+    {
+        Shoot(false);
+
+        if (target != null)
+        {
+            float distance = Vector3.Distance(gameObject.transform.position, target.transform.position);
+            if(distance < Range)
+            {
+                Shoot(true);
+                top.transform.LookAt(target.transform);
+            }            
+        }
+    }
+
+    private void Shoot(bool v)
+    {
+        var emissionModule = Guns.emission;
+        emissionModule.enabled = v;
+    }
+
+    private void FindTraget()
+    {
+        target = null;
+        List<Enemy> EnemyList = FindObjectsOfType<Enemy>().ToList();
+        if (EnemyList.Count == 0) { return; }
+        EnemyList.OrderBy(o => Vector3.Distance(gameObject.transform.position, o.transform.position));
+        target = EnemyList[0]; //Closest Traget.
+    }
 }
